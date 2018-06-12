ed74d08c6e9a4601ebc6670cc4cb8abc6e7d51bd
Gianni Georg Weinand <gi4nn1@yahoo.de>
2018-06-12 13:28:59 -0300

    Comienzo de la implementacion del tateti.

--- tp.erl
+++ tp.erl
@@ -2,7 +2,45 @@
 -compile(export_all).
 
 %~ NameTable: Inserta tuplas unitarias con los nombres de jugador.
-%~ GameTable: Inserts tuplas de la forma {juegoID, tablero, jugadores, spectators}.
+%~ GameTable: Inserts tuplas de la forma {juegoID, tablero, turno, jugadores, spectators}.
+
+
+newGame(GameTable) ->
+	%~ id, tablero, turno, jugadores, espectadores
+	dets:insert(GameTable, {getGameID(), newBoard(), 0, {}, {}}).
+
+getGameID() ->
+	%~ dummy pls fix
+	1.
+
+newBoard() ->
+    {vacio, vacio, vacio,
+     vacio, vacio, vacio,
+     vacio, vacio, vacio}.
+     
+changeTurn(Turn) ->
+	case Turn of
+		0 -> 1;
+		1 -> 0 end.
+		
+turnFig(Turn) ->
+	case Turn of
+		0 -> x;
+		1 -> o end.
+     
+makeMove(DaddyID, GameTable, GameID, CurrentPlayer, Pos) ->
+	case dets:lookup(GameTable, GameID) of
+		[] -> DaddyID ! {error, "ERROR Juego no encontrado\n"};
+		{GameID, Board, Turn, Players, Spectators} ->
+			case element(Turn, Players) of
+				CurrentPlayer ->
+					case element(Pos, Board) of ->
+						vacio -> dets:insert(GameTable, {ok, {GameID, setelement(Pos, Board, turnFig(Turn)), changeTurn(Turn), Players, Spectators}),
+								 DaddyID ! {ok, "Tablero updateado correctamente\n"}
+						_ -> DaddyID ! {error, "ERROR Posicion previamente ocupada\n"} end;
+				_ -> DaddyID ! {error, "ERROR No es tu turno, tramposo!\n"} end;	
+		%~ CAMBIAR ESTE MENSAJE ANTES DE ENTREGAR
+		_ -> DaddyID ! {error, "ERROR Instructions unclear, dick stuck in ceiling fan\n"} end.
 
 
 init() ->
@@ -18,48 +56,42 @@ init() ->
 dispatcher(ListenSocket, NameTable, GameTable) ->
 					{ok, Socket} = gen_tcp:accept(ListenSocket),
 					spawn(?MODULE, dispatcher, [ListenSocket, NameTable, GameTable]),
-					pSocketLogin(Socket, NameTable, GameTable).
-
+					psocket(Socket, NameTable, GameTable).
 	
-pSocketLogin(Socket, NameTable, GameTable) -> 
-	receive {tcp, Socket, Msg} -> 	spawn(?MODULE, pcomandoLogin, [self(), Msg, NameTable]),
-									pSocketLogin(Socket, NameTable, GameTable);
-			{rambo, ok, Msg, Username} -> gen_tcp:send(Socket, Msg), psocket(Socket, NameTable, GameTable, Username);
-			{rambo, error, Msg} -> gen_tcp:send(Socket, Msg), pSocketLogin(Socket, NameTable, GameTable);
-			{close} -> gen_tcp:close(Socket);
-			_ -> io:format("Woops ~n") end.
-
-			
-pcomandoLogin(DaddyID, Msg, NameTable) ->
-	MsgList = string:tokens(string:trim(Msg), " "),
-	case lists:nth(1, MsgList) of
-		"BYE" ->DaddyID ! {close};
-		"CON" ->Username = lists:nth(2, MsgList),
-				case dets:lookup(NameTable, Username) of
-					[] -> 	dets:insert(NameTable, {Username}), 
-							DaddyID ! {rambo, ok, "OK\n", Username};
-					_ -> DaddyID ! {rambo, error, "ERROR Nombre ya existente\n"} end;
-		_ -> 	DaddyID ! {rambo, error, "ERROR Comando no válido. Inicie sesión con CON NombreDeUsuario\r\n"} end.
 
 
  
-psocket(Socket, NameTable, GameTable, Username) -> 
-	receive {tcp, Socket, Msg} -> 	spawn(?MODULE, pcomando, [self(), Msg, NameTable, GameTable]), 
-									psocket(Socket, NameTable, GameTable, Username);
-			{rambo, Msg} -> gen_tcp:send(Socket, Msg), psocket(Socket, NameTable, GameTable, Username);
-			{close} -> gen_tcp:close(Socket);
-			_ -> io:format("Woops ~n") end.
+psocket(Socket, NameTable, GameTable) -> receive {tcp, Socket, Msg} -> 	spawn(?MODULE, pcomando, [self(), Msg, NameTable, GameTable]), 
+																		psocket(Socket, NameTable, GameTable);
+						   {rambo, Msg} -> gen_tcp:send(Socket, Msg), psocket(Socket, NameTable, GameTable);
+						   {close} -> gen_tcp:close(Socket);
+						   _ -> io:format("Woops ~n") end.
+						   
 
 
 pcomando(DaddyID, Msg, NameTable, GameTable) ->
-	MsgList = string:tokens(string:trim(Msg), " "),
+	MsgList = string:tokens(Msg, " "),
 	case lists:nth(1, MsgList) of
 		"BYE" -> DaddyID ! {close};
 		
-		"CON" -> 	DaddyID ! {rambo, "ERROR Sesión ya iniciada\n"};
+		"CON" -> 	Username = lists:nth(2, MsgList),
+					case dets:lookup(NameTable, Username) of
+					[] -> 	dets:insert(NameTable, {Username}), 
+							DaddyID ! {rambo, "OK\n"};
+					_ -> DaddyID ! {rambo, "ERROR Nombre ya existente\n"} end;
 					
 		"FIND" -> 	case dets:lookup(NameTable, lists:nth(2, MsgList)) of
 					[] -> DaddyID ! {rambo, "N I S M A N E A D O\n"};
 					_ -> DaddyID ! {rambo, "Acata\n"} end;
+		
+		%~ dummy pls fix		
+		"NEW" ->	newGame(GameTable);
 				
-		_ -> DaddyID ! {rambo, "ERROR Comando no válido\n"} end.
+		"PLA" ->	%~ Arreglar GameID
+					GameID = 1,
+					Pos = lists:nth(3, MsgList),
+					spawn(?MODULE, makeMove, [self(), GameTable, GameID, Username, Pos]),
+					%~ Refinar esto pls
+					receive {_, Msg} -> DaddyID ! {rambo, Msg};
+		_ -> DaddyID ! {rambo, Msg} end.
+
