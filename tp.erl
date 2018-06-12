514ea92d05e36855f583b4c90e339e889322eb86
Tomasfdel <Tomasfdel@users.noreply.github.com>
2018-06-12 13:27:16 -0300

    Agregamos psocket especifico para login

--- tp.erl
+++ tp.erl
@@ -18,32 +18,48 @@ init() ->
 dispatcher(ListenSocket, NameTable, GameTable) ->
 					{ok, Socket} = gen_tcp:accept(ListenSocket),
 					spawn(?MODULE, dispatcher, [ListenSocket, NameTable, GameTable]),
-					psocket(Socket, NameTable, GameTable).
+					pSocketLogin(Socket, NameTable, GameTable).
+
 	
+pSocketLogin(Socket, NameTable, GameTable) -> 
+	receive {tcp, Socket, Msg} -> 	spawn(?MODULE, pcomandoLogin, [self(), Msg, NameTable]),
+									pSocketLogin(Socket, NameTable, GameTable);
+			{rambo, ok, Msg, Username} -> gen_tcp:send(Socket, Msg), psocket(Socket, NameTable, GameTable, Username);
+			{rambo, error, Msg} -> gen_tcp:send(Socket, Msg), pSocketLogin(Socket, NameTable, GameTable);
+			{close} -> gen_tcp:close(Socket);
+			_ -> io:format("Woops ~n") end.
+
+			
+pcomandoLogin(DaddyID, Msg, NameTable) ->
+	MsgList = string:tokens(string:trim(Msg), " "),
+	case lists:nth(1, MsgList) of
+		"BYE" ->DaddyID ! {close};
+		"CON" ->Username = lists:nth(2, MsgList),
+				case dets:lookup(NameTable, Username) of
+					[] -> 	dets:insert(NameTable, {Username}), 
+							DaddyID ! {rambo, ok, "OK\n", Username};
+					_ -> DaddyID ! {rambo, error, "ERROR Nombre ya existente\n"} end;
+		_ -> 	DaddyID ! {rambo, error, "ERROR Comando no válido. Inicie sesión con CON NombreDeUsuario\r\n"} end.
 
 
  
-psocket(Socket, NameTable, GameTable) -> receive {tcp, Socket, Msg} -> 	spawn(?MODULE, pcomando, [self(), Msg, NameTable, GameTable]), 
-																		psocket(Socket, NameTable, GameTable);
-						   {rambo, Msg} -> gen_tcp:send(Socket, Msg), psocket(Socket, NameTable, GameTable);
-						   {close} -> gen_tcp:close(Socket);
-						   _ -> io:format("Woops ~n") end.
-						   
+psocket(Socket, NameTable, GameTable, Username) -> 
+	receive {tcp, Socket, Msg} -> 	spawn(?MODULE, pcomando, [self(), Msg, NameTable, GameTable]), 
+									psocket(Socket, NameTable, GameTable, Username);
+			{rambo, Msg} -> gen_tcp:send(Socket, Msg), psocket(Socket, NameTable, GameTable, Username);
+			{close} -> gen_tcp:close(Socket);
+			_ -> io:format("Woops ~n") end.
 
 
 pcomando(DaddyID, Msg, NameTable, GameTable) ->
-	MsgList = string:tokens(Msg, " "),
+	MsgList = string:tokens(string:trim(Msg), " "),
 	case lists:nth(1, MsgList) of
 		"BYE" -> DaddyID ! {close};
 		
-		"CON" -> 	Username = lists:nth(2, MsgList),
-					case dets:lookup(NameTable, Username) of
-					[] -> 	dets:insert(NameTable, {Username}), 
-							DaddyID ! {rambo, "OK\n"};
-					_ -> DaddyID ! {rambo, "ERROR Nombre ya existente\n"} end;
+		"CON" -> 	DaddyID ! {rambo, "ERROR Sesión ya iniciada\n"};
 					
 		"FIND" -> 	case dets:lookup(NameTable, lists:nth(2, MsgList)) of
 					[] -> DaddyID ! {rambo, "N I S M A N E A D O\n"};
 					_ -> DaddyID ! {rambo, "Acata\n"} end;
 				
-		_ -> DaddyID ! {rambo, Msg} end.
+		_ -> DaddyID ! {rambo, "ERROR Comando no válido\n"} end.
